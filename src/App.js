c2eec2e6fc8d7f02d5fa274a05358b3a07dc9aa2
Diego Quinones <diegoqr2000@gmail.com>
2021-10-01 21:34:21 -0400

    F2. Se puede enviar una cadena de numeros para que se sume

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function CalculadoraCadenas(numero) {
-  return parseInt(numero);
+function CalculadoraCadenas(numeros) {
+  var suma = 0;
+  numeros.split(',').forEach(numero => {
+    suma = suma + parseInt(numero);
+  });
+  return suma;
 }
 
 export {CalculadoraCadenas};
--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,7 @@ describe("CalculadoraCadenas", () => {
   it("deberia devolver un numero", () => {
     expect(CalculadoraCadenas("3")).toEqual(3);
   });
+  it("deberia devolver la suma de una cadena de numeros", () => {
+    expect(CalculadoraCadenas("1,2,4,8")).toEqual(15);
+  });
 });
